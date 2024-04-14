8aca1012a8ee3a045c91ab22c12c40a0d3b6725a
Yurii <iyureg@gmail.com>
2024-04-14 17:05:54 +0300

    modify: sem2 homework / encapsulation db queries in config file with annotation ConfigurationProperties

--- SpringGeekBrains/Webinar_2_spring_base_crud/homework/src/main/java/ru/gb/homeworkSpringCRUD/Empl4sem2CrudApplication.java
+++ SpringGeekBrains/Webinar_2_spring_base_crud/homework/src/main/java/ru/gb/homeworkSpringCRUD/Empl4sem2CrudApplication.java
@@ -2,8 +2,10 @@ package ru.gb.homeworkSpringCRUD;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class Empl4sem2CrudApplication {
 
 	public static void main(String[] args) {
